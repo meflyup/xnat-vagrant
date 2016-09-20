ac2c1767ee49025675e261968871dae2183ad387
Rick Herrick <rherrick@gmail.com>
2016-09-20 10:53:32 -0500

    1.7.0 release

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -6,7 +6,7 @@ echo Now running the "defaults.sh" provisioning script.
 [[ -z $DEPLOY ]] && { DEPLOY=release; echo s/@DEPLOY@/release/g >> /vagrant/.work/vars.sed; }
 [[ -z $PROJECT ]] && { PROJECT=xnat; echo s/@PROJECT@/xnat/g >> /vagrant/.work/vars.sed; }
 [[ -z $SITE ]] && { SITE=XNAT; echo s/@SITE@/XNAT/g >> /vagrant/.work/vars.sed; }
-[[ -z $REVISION ]] && { REVISION=1.7.0-SNAPSHOT; echo s/@REVISION@/1.7.0-SNAPSHOT/g >> /vagrant/.work/vars.sed; }
+[[ -z $REVISION ]] && { REVISION=1.7.0; echo s/@REVISION@/1.7.0/g >> /vagrant/.work/vars.sed; }
 [[ -z $XNAT_REV ]] && { XNAT_REV=$REVISION; echo s/@XNAT_REV@/$REVISION/g >> /vagrant/.work/vars.sed; }
 [[ -z $PIPELINE_REV ]] && { PIPELINE_REV=$REVISION; echo s/@PIPELINE_REV@/$REVISION/g >> /vagrant/.work/vars.sed; }
 [[ -z $XNAT ]] && { XNAT=xnat-${XNAT_REV}; echo s/@XNAT@/xnat-${XNAT_REV}/g >> /vagrant/.work/vars.sed; }
